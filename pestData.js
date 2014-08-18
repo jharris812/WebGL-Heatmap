771ff3dc8083407d2106ebefe7d3580cd1fd72b1
jharris812 <jharris812@gmail.com>
2014-08-18 19:09:50 -0400

    Delaunay triangulation & rendering with color
    Basic first step, creating and rendering the data as triangles with
    grayscale color.

--- pestData.js
+++ pestData.js
@@ -1,3 +1,6 @@
+
+
+
 var pestData = [{"pest":"California Red Scale","B":"CRS","x":35.32561248,"y":-118.8587236,"date":"3/29/2014 7:00","severity":5,"G":1393},
 {"pest":"California Red Scale","B":"CRS","x":35.33291252,"y":-118.8699567,"date":"3/29/2014 7:00","severity":5,"G":1394},
 {"pest":"California Red Scale","B":"CRS","x":35.32571753,"y":-118.8566637,"date":"3/29/2014 7:00","severity":10,"G":1393},
@@ -9,7 +12,6 @@ var pestData = [{"pest":"California Red Scale","B":"CRS","x":35.32561248,"y":-11
 {"pest":"California Red Scale","B":"CRS","x":35.33965177,"y":-118.8649786,"date":"3/29/2014 7:00","severity":10,"G":1394},
 {"pest":"California Red Scale","B":"CRS","x":35.33967803,"y":-118.8693881,"date":"3/29/2014 7:00","severity":10,"G":1394},
 {"pest":"California Red Scale","B":"CRS","x":35.32587509,"y":-118.8612771,"date":"3/29/2014 7:00","severity":10,"G":1394},
-{"pest":"California Red Scale","B":"CRS","x":35.03449433,"y":-118.9889717,"date":"3/29/2014 7:00","severity":10,"G":1395},
 {"pest":"California Red Scale","B":"CRS","x":35.32512229,"y":-118.861084,"date":"3/29/2014 7:00","severity":10,"G":1483},
 {"pest":"California Red Scale","B":"CRS","x":35.33256241,"y":-118.8587236,"date":"3/29/2014 7:00","severity":15,"G":1393},
 {"pest":"California Red Scale","B":"CRS","x":35.33657979,"y":-118.861202,"date":"3/29/2014 7:00","severity":15,"G":1394},
@@ -18,7 +20,6 @@ var pestData = [{"pest":"California Red Scale","B":"CRS","x":35.32561248,"y":-11
 {"pest":"California Red Scale","B":"CRS","x":35.33903914,"y":-118.8604403,"date":"3/29/2014 7:00","severity":20,"G":1393},
 {"pest":"California Red Scale","B":"CRS","x":35.33987932,"y":-118.8541532,"date":"3/29/2014 7:00","severity":20,"G":1393},
 {"pest":"California Red Scale","B":"CRS","x":35.33615093,"y":-118.8739049,"date":"3/29/2014 7:00","severity":20,"G":1394},
-{"pest":"California Red Scale","B":"CRS","x":35.03735814,"y":-118.985281,"date":"3/29/2014 7:00","severity":20,"G":1395},
 {"pest":"California Red Scale","B":"CRS","x":35.32405436,"y":-118.8702679,"date":"3/29/2014 7:00","severity":20,"G":1483},
 {"pest":"California Red Scale","B":"CRS","x":35.32573503,"y":-118.8543248,"date":"3/29/2014 7:00","severity":25,"G":1393},
 {"pest":"California Red Scale","B":"CRS","x":35.33293002,"y":-118.8582945,"date":"3/29/2014 7:00","severity":25,"G":1393},
@@ -29,7 +30,6 @@ var pestData = [{"pest":"California Red Scale","B":"CRS","x":35.32561248,"y":-11
 {"pest":"California Red Scale","B":"CRS","x":35.33854903,"y":-118.8522005,"date":"3/29/2014 7:00","severity":30,"G":1393},
 {"pest":"California Red Scale","B":"CRS","x":35.336092,"y":-118.868849,"date":"3/29/2014 7:00","severity":30,"G":1394},
 {"pest":"California Red Scale","B":"CRS","x":35.3325099,"y":-118.8649678,"date":"3/29/2014 7:00","severity":30,"G":1394},
-{"pest":"California Red Scale","B":"CRS","x":35.04106513,"y":-118.9933276,"date":"3/29/2014 7:00","severity":30,"G":1395},
 {"pest":"California Red Scale","B":"CRS","x":35.32198848,"y":-118.8610625,"date":"3/29/2014 7:00","severity":30,"G":1483},
 {"pest":"California Red Scale","B":"CRS","x":35.33620345,"y":-118.8650751,"date":"3/29/2014 7:00","severity":40,"G":1394},
 {"pest":"California Red Scale","B":"CRS","x":35.32889496,"y":-118.8650215,"date":"3/29/2014 7:00","severity":45,"G":1394},
@@ -37,9 +37,19 @@ var pestData = [{"pest":"California Red Scale","B":"CRS","x":35.32561248,"y":-11
 {"pest":"California Red Scale","B":"CRS","x":35.3321948,"y":-118.8522863,"date":"3/29/2014 7:00","severity":50,"G":1393},
 {"pest":"California Red Scale","B":"CRS","x":35.33896912,"y":-118.8560629,"date":"3/29/2014 7:00","severity":50,"G":1393},
 {"pest":"California Red Scale","B":"CRS","x":35.32933261,"y":-118.8612664,"date":"3/29/2014 7:00","severity":50,"G":1394},
-{"pest":"California Red Scale","B":"CRS","x":35.03445919,"y":-118.9917827,"date":"3/29/2014 7:00","severity":50,"G":1395},
 {"pest":"California Red Scale","B":"CRS","x":35.32340659,"y":-118.8690877,"date":"3/29/2014 7:00","severity":55,"G":1483},
 {"pest":"California Red Scale","B":"CRS","x":35.32310896,"y":-118.8612986,"date":"3/29/2014 7:00","severity":55,"G":1483},
 {"pest":"California Red Scale","B":"CRS","x":35.32520983,"y":-118.8661265,"date":"3/29/2014 7:00","severity":65,"G":1483},
 {"pest":"California Red Scale","B":"CRS","x":35.32190094,"y":-118.8694096,"date":"3/29/2014 7:00","severity":75,"G":1483},
 {"pest":"California Red Scale","B":"CRS","x":35.32386178,"y":-118.861084,"date":"3/29/2014 7:00","severity":90,"G":1483}];
+
+
+/*
+
+var pestData = [
+{"pest":"Cherry Codling Moth","B":"ChCM","x":1,"y":1,"date":"5/8/2013 6:51","F":3,"severity":1},
+{"pest":"Cherry Codling Moth","B":"ChCM","x":2,"y":2,"date":"5/8/2013 6:51","F":3,"severity":.3},
+{"pest":"Cherry Codling Moth","B":"ChCM","x":2,"y":-1,"date":"5/8/2013 6:51","F":3,"severity":.4},
+{"pest":"Cherry Codling Moth","B":"ChCM","x":4,"y":0,"date":"5/8/2013 6:51","F":3,"severity":.1}
+];
+*/
